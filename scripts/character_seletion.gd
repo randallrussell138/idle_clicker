c4e3d7ebfbb3f3464cd7256fd98854538f84cfa9
Randall J. Russell Hernadez <rjrussellh03@gmail.com>
2024-05-10 18:21:17 -0400

    new select screen

--- scripts/character_seletion.gd
+++ scripts/character_seletion.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var character = ""
 func _on_black_cat_pressed():
 	Global.character = "black_cat"
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
@@ -14,3 +14,7 @@ func _on_orange_cat_pressed():
 func _on_white_cat_pressed():
 	Global.character = "white_cat"
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_texture_button_pressed():
+	get_tree().quit()
--- scripts/game.gd
+++ scripts/game.gd
@@ -4,3 +4,7 @@ var current_scene = "game"
 var change_scene = false
 var scene_path = ""
 
+
+
+func _on_exit_pressed():
+	get_tree().change_scene_to_file("res://scenes/character_seletion.tscn")
